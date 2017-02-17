e206ecda66d8c6392f0db5f69df584dff164c4dc
Bachynin Ivan <bachynin.i@gmail.com>
2017-02-17 15:08:54 +0200

    🚑 fix title printer

--- utils_pack/printers.py
+++ utils_pack/printers.py
@@ -3,14 +3,14 @@ from utils_pack.measurments import *
 
 def print_title(title: str, length=89):
     half = (length - len(title) - 2) // 2
-    corrector = 1 if len(title) % 2 == 0 else 0
+    corrector = 1 if len(title) % 2 != length % 2 else 0
     print(f"{'#'*length}")
     print(f"{'#'*half} {title} {'#'*(half + corrector)}")
     print(f"{'#'*length}\n")
 
 
-def print_header(header: str, length=88):
-    print(f"+{'-'*length}\n"
+def print_header(header: str, length=89):
+    print(f"+{'-'*(length-1)}\n"
           f"| {header}\n"
           f"+{'-'*(length-1)}\n")
 
