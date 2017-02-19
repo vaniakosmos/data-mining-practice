29467f0b6c59bba587df4b64f2f87adaee52f7fc
Bachynin Ivan <bachynin.i@gmail.com>
2017-02-19 15:31:54 +0200

    ✨ add kind of "k-means" classification

--- lab3/k_means.py
+++ lab3/k_means.py
@@ -1,8 +1,40 @@
 from lab3.utils import Data
 
 
+def predict(vector, groups):
+    chosen_class, min_changes = None, None
+    for index, group in enumerate(groups):
+        sum_vector, size = group
+        old_center = [e / size for e in sum_vector]
+        new_center = [(a + b) / (size + 1) for a, b in zip(sum_vector, vector)]
+
+        change = sum([pow(a-b, 2) for a, b in zip(old_center, new_center)])
+
+        if min_changes is None or change < min_changes:
+            chosen_class = index
+            min_changes = change
+    return chosen_class
+
+
+def calculate_sums(data: Data):
+    groups = [[[0 for _ in range(len(data[0])-1)], 0] for _ in range(data.classes_number)]
+    for instance in data:
+        vector, class_id = instance[:-1], instance[-1]
+        sum_vector, size = groups[class_id]
+        sum_vector = [a+b for a, b in zip(sum_vector, vector)]
+        size += 1
+        groups[class_id] = sum_vector, size
+    return groups
+
+
 def evaluate(train: Data, test: Data):
-    """
-    https://onlinecourses.science.psu.edu/stat857/node/113
-    """
-    pass
+    confusion_matrix = [[0] * train.classes_number for _ in range(train.classes_number)]
+
+    groups = calculate_sums(train)
+    for instance in test:
+        vector, class_id = instance[:-1], instance[-1]
+        prediction = predict(vector, groups)
+
+        confusion_matrix[class_id][prediction] += 1
+
+    return confusion_matrix
--- lab3/main.py
+++ lab3/main.py
@@ -8,12 +8,14 @@
 3. методу найближчого сусіда
 Навчити та оцінити точність класифікатора принаймни на трьох бд (https://archive.ics.uci.edu/ml/datasets.html).
 """
-from lab3 import k_nearest_neighbours, decision_tree
+from lab3 import k_nearest_neighbours, decision_tree, k_means
 from utils_pack.printers import *
 from lab3.utils import Data
 
 
 def main():
+    headers_length = 50
+
     data_sets = (
         ("IRIS", "./data_sets/iris.csv"),
         ("WINE", "./data_sets/wine.csv"),
@@ -26,15 +28,15 @@ def main():
         data_set = Data(csv=file)
         train, test = data_set.split_randomly(ratio=0.7)
 
-        print_header("Decision Tree", length=50)
+        print_header("Decision Tree", length=headers_length)
         confusion_matrix = decision_tree.evaluate(train, test, max_depth=11, print_out_tree=False)
         print_results(confusion_matrix)
 
-        # print_header("K-Means", length=50)
-        # confusion_matrix = k_means.evaluate(train, test)
-        # print_results(confusion_matrix)
+        print_header("K-Means", length=headers_length)
+        confusion_matrix = k_means.evaluate(train, test)
+        print_results(confusion_matrix)
 
-        print_header("K-Nearest Neighbours", length=50)
+        print_header("K-Nearest Neighbours", length=headers_length)
         confusion_matrix = k_nearest_neighbours.evaluate(train, test, neighbours_number=3)
         print_results(confusion_matrix)
 
